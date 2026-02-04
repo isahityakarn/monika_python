a88ba07cefa7a73ae039d32265a77165df38de8a
sahityakarn <45307170+isahityakarn@users.noreply.github.com>
2026-02-04 20:59:14 +0530

    Add inputData.py to calculate the sum of two user-provided numbers

---
+++ inputData.py
@@ -0,0 +1,9 @@
+# num=50
+# num2=20
+# total=num+num2
+# print("The total is:", total)
+
+num=int(input("Enter a number: "))
+num2=int(input("Enter another number: ")) 
+total=num+num2
+print("The total is:", total)
