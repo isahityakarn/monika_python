8e274147d3bcbfaaa457a8973f579af51101c4e6
sahityakarn <45307170+isahityakarn@users.noreply.github.com>
2026-02-24 20:20:28 +0530

    if condition

---
+++ if_condition.py
@@ -0,0 +1,44 @@
+#syntex
+# if condition:
+#     result
+# else:
+#     result
+# Q1. find out whether a person is eligible for voting or not
+# age>=18 eligible
+# not eligible
+
+# Q2. find out account to grade marks>=90 grade A
+# marks>=80 grade B 
+
+# Q3. find out account to grade 
+# marks>=90 grade A
+# marks>=80 grade B 
+#marks>=70 grade C
+# marks>=60 grade D
+# marks<60 grade F
+
+
+
+# age=int(input("Enter your age"))
+# if age>=18:
+#     print("you are eligible for voting")
+# else:
+#     print("you are not eligible for voting")
+
+# marks=int(input("Enter your marks"))
+# if marks>=90:
+#     print("grade A")    
+# else:
+#     print("grade B")
+
+# marks=int(input("Enter your marks"))
+# if marks>=90:
+#     print("grade A")
+# elif marks>=80:
+#     print("grade B")
+# elif marks>=70:
+#     print("grade C")
+# elif marks>=60:
+#     print("grade D")
+# else:
+#     print("grade F")
